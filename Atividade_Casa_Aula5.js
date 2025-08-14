ce90e1e7e1e80e9deb9dd4eac1ff73c3b8c7221d
fabinhosvc <fabio.souto@gmail.com>
2025-08-14 10:52:24 -0300

    Atividade_Casa_Aula5.js

---
+++ Atividade_Casa_Aula5.js
@@ -0,0 +1,15 @@
+let num1 = 10;
+let num2 = 5;
+let operador = "+"
+
+if (operador === "+" ) {
+    console.log("A soma dos números é:", num1 + num2);
+} else if (operador === "-") {
+    console.log("A subtração dos números é:", num1 - num2);
+} else if (operador === "*") {
+    console.log("A multiplicação dos números é:", num1 * num2);
+} else if (operador === "/") {
+    console.log("A divisão dos números é:", num1 / num2);
+} else {
+    console.log("Operador Inválido");
+}
